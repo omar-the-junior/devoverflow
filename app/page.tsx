292ae14dab41ef7fd5321b0f80b61015a3b2ee6f
omar-the-junior <omarmasoudonline@gmail.com>
2024-12-01 15:51:19 +0200

    chore: ESlin & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <h1>Hello from Next.js</h1>
+      <h1 className="text-3xl font-black text-white">
+        Welcome to the world of Nex.js
+      </h1>
     </>
   );
 }
