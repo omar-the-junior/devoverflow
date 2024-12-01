1d1e88c034a7c8452767c90eca7d701ca2fedeb3
omar-the-junior <omarmasoudonline@gmail.com>
2024-12-01 16:48:09 +0200

    chore: add assets and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <>
-      <h1 className="h1-bold">Welcome to the world of Nex.js</h1>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to the world of Next.js
+      </h1>
     </>
   );
 }
