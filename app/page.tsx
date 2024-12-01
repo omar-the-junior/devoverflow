b877dab73985e34d72644f6e45159d86fb506ccb
omar-the-junior <omarmasoudonline@gmail.com>
2024-12-01 16:12:08 +0200

    tailwindcss setup & theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-black text-white">
-        Welcome to the world of Nex.js
-      </h1>
+      <h1 className="h1-bold">Welcome to the world of Nex.js</h1>
     </>
   );
 }
